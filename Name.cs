cae5146f38d80b048999be74cea5374d12e53dcf
Dell-User <Dell-User@DESKTOP-8EU8MHV>
2023-10-11 08:45:05 +0200

    Add project files.

---
+++ Name.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Static_Methods_and_Classes
+{
+    static internal class UseFullTools
+    {
+        public static void sayHi(String name)
+        {
+            Console.WriteLine("Hi" + name);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Static_Methods_and_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Math.Sqrt(144));
+
+            UseFullTools.sayHi("Mokgadi");
+
+            //freeze
+            Console.ReadLine();
+        }
+    }
+}
